9574bcfffc90490548e356fc088837291765329a
“Pascwin” <pascal-winner@web.de>
2023-01-08 21:10:30 +0100

    added a form to manage opt input with good ux

--- src/components/CustomLink.jsx
+++ src/components/CustomLink.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const CustomLink = ({ to, children }) => {
-    return (
-        <Link className='text-dark-subtle hover:text-white transition'
-            to={to}>{children}</Link>
-    )
-}
+  return (
+    <Link className="text-dark-subtle hover:text-white transition" to={to}>
+      {children}
+    </Link>
+  );
+};
--- src/components/auth/EmailVerification.jsx
+++ src/components/auth/EmailVerification.jsx
@@ -1,7 +1,79 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
+import { Container } from "../Container";
+import { Submit } from "../form/Submit";
+import { Title } from "../form/Title";
+
+const OTP_LENGHT = 6;
+let currentOTPIndex;
 
 export const EmailVerification = () => {
+  const [otp, setOtp] = useState(new Array(OTP_LENGHT).fill(""));
+  const [activeOtpIndex, setActiveOtpIndex] = useState(0);
+
+  const inputRef = useRef();
+
+  const focusNextInputField = (index) => {
+    setActiveOtpIndex(index + 1);
+  };
+
+  const focusPrevInputField = (index) => {
+    let nextIndex;
+    const diff = index - 1;
+    nextIndex = diff !== 0 ? diff : 0;
+    setActiveOtpIndex(nextIndex);
+  };
+
+  const handleOtpChange = ({ target }) => {
+    const { value } = target;
+    const newOtp = [...otp];
+    newOtp[currentOTPIndex] = value.substring(value.length - 1, value.length);
+
+    if (!value) focusPrevInputField(currentOTPIndex);
+    else focusNextInputField(currentOTPIndex);
+
+    setOtp([...newOtp]);
+  };
+
+  const handleKeyDown = ({ key }, index) => {
+    currentOTPIndex = index;
+    if (key === "Backspace") {
+      focusPrevInputField(currentOTPIndex);
+    }
+  };
+
+  useEffect(() => {
+    inputRef.current?.focus();
+  }, [activeOtpIndex]);
+
   return (
-    <div>EmailVerification</div>
-  )
-}
+    <div className="fixed inset-0 bg-primary -z-10 flex justify-center items-center">
+      <Container>
+        <form className="bg-secondary rounded p-6 space-y-6">
+          <div>
+            <Title>Please Enter the OTP to verify your account</Title>
+            <p className="text-center text-dark-subtle">
+              OTP has been sent to your email
+            </p>
+          </div>
+          <div className="flex justify-center items-center space-x-4">
+            {otp.map((_, index) => {
+              return (
+                <input
+                  ref={activeOtpIndex === index ? inputRef : null}
+                  key={index}
+                  type="number"
+                  value={otp[index] || ""}
+                  onChange={handleOtpChange}
+                  onKeyDown={(e) => handleKeyDown(e, index)}
+                  className="w-12 h-12 border-2 border-dark-subtle focus:border-white rounded bg-transparent 
+                  outline-none text-center text-white font-semibold text-xl transition"
+                />
+              );
+            })}
+          </div>
+          <Submit value="Send Link" />
+        </form>
+      </Container>
+    </div>
+  );
+};
