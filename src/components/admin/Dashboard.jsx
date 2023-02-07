82be27961d66aee24cf8620c025c5b47327ee2cb
“Pascwin” <pascal-winner@web.de>
2023-02-07 21:41:58 +0100

    navbar fixed and visible everywhere

--- src/components/admin/Dashboard.jsx
+++ src/components/admin/Dashboard.jsx
@@ -1,59 +1,7 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
-import { AiOutlineHome } from "react-icons/ai";
-import { BiMoviePlay } from "react-icons/bi";
-import { FaUserNinja } from "react-icons/fa";
-import { FiLogOut } from "react-icons/fi";
+import React from 'react'
 
 export const Dashboard = () => {
   return (
-    <nav className="w-48 min-h-screen bg-secondary border-r border-gray-300 flex flex-col justify-between">
-      <ul className="pl-5">
-        <li className="mb-8">
-          <Link to="/">
-            <img src="./logo.png" alt="logo" className="h-14 p-2"/>
-          </Link>
-        </li>
-        <li>
-          <NavItem to="/">
-            <AiOutlineHome />
-            <span>Home</span>
-          </NavItem>
-        </li>
-        <li>
-          <NavItem to="/movies">
-            <BiMoviePlay />
-            <span>Movies</span>
-          </NavItem>
-        </li>
-        <li>
-          <NavItem to="/actors">
-            <FaUserNinja />
-            <span>Actors</span>
-          </NavItem>
-        </li>
-      </ul>
-      <div className="flex flex-col items-start text-white p-5">
-        <span className="font-semibold text-white text-xl">Admin</span>
-        <button className="flex items-center text-dark-subtle text-sm hover:text-white transition space-x-0">
-          <FiLogOut />
-          <span>Logout</span>
-        </button>
-      </div>
-    </nav>
-  );
-};
-
-const NavItem = ({ children, to }) => {
-  const commonClasses =
-    " flex items-center text-lg space-x-2 p-2 hover:opacity-80";
-  return (
-    <NavLink
-      className={({ isActive }) =>
-        (isActive ? "text-white" : "text-gray-400") + commonClasses
-      }
-      to={to}>
-      {children}
-    </NavLink>
-  );
-};
+    <div>Dashboard</div>
+  )
+}
